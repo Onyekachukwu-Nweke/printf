100cb11ec2fdeab47958b33fd46c314fcc9051d8
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-18 16:03:30 -0700

    added some prototypes into the header file

--- main.h
+++ main.h
@@ -1,6 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+#include <stdarg.h>
+int printIdentifiers(char next, va_list arg);
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_int(va_list arg);
+int print_char(va_list arg);
+int print_STR(va_list arg);
+int print_str(va_list arg);
 
+typedef struct identifierStruct
+{
+	char *identifier;
+	int (*printer)(va_list);
+} identifierStruct;
 #endif
